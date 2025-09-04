bf7a3c76753d4ddd75161d9c0f3fcbe67fa07e94
jomrembau <jomir.bautista@gmail.com>
2025-09-04 18:58:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,19 @@ from turtle import Screen
 from score import Score
 from food import Food
 from snake import Snake
-import time
 
-game_on = True
-
+first_move = True
 win = Screen()
+win.title("Snake Game")
+win.bgcolor("black")
+win.setup(600, 600)
+win.tracer(0)
+
 score = Score()
 food = Food()
 snake = Snake()
-win.title("Snake Game")
-win.bgcolor("black")
-win.tracer(0)
-win.setup(600,600)
-
 snake.create_snake()
+snake.snake_body[0].setheading(0)  # head faces right
 
 win.listen()
 win.onkey(snake.move_up, "Up")
@@ -23,19 +22,25 @@ win.onkey(snake.move_down, "Down")
 win.onkey(snake.move_left, "Left")
 win.onkey(snake.move_right, "Right")
 
-while game_on:
+def game_loop():
+    global first_move
     snake.move_snake()
     win.update()
-    time.sleep(0.1)
 
-    x_outside = snake.snake_body[0].xcor() >= 280 or snake.snake_body[0].xcor() <= -280
-    y_outside = snake.snake_body[0].ycor() >= 280 or snake.snake_body[0].ycor() <= -280
-
-    if x_outside or y_outside:
-        game_on = False
+    if abs(snake.snake_body[0].xcor()) >= 270 or abs(snake.snake_body[0].ycor()) >= 270:
+        print("Game Over: Hit Wall")
+        return
+    if not first_move and snake.body_collision():
+        print("Game Over: Hit Body")
+        return
 
     if snake.snake_body[0].distance(food.snake_food) < 15:
         food.food_refresh()
         score.update_score()
+        snake.add_segment()
 
+    first_move = False
+    win.ontimer(game_loop, 100)
+
+game_loop()
 win.mainloop()
